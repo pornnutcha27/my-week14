686d6b66abd9967a7a43f370cefa426d483a0ddf
pornnutcha27 <kadplas96@gmail.com>
2026-09-07 15:34:19 +0700

    ok

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('title')
+    หน้าแรกของเว็บไซต์
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+        <h1 class="display-6">ยินดีต้อนรับสู่ Blog Application</h1>
+        <p class="lead mb-0">ระบบสำหรับอ่านและจัดการบทความ โดยผู้ใช้สามารถเขียน แก้ไข และเผยแพร่บทความได้หลังเข้าสู่ระบบ</p>
+    </div>
+</div>
+@endsection
